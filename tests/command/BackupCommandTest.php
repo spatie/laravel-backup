cbf28caaffa710479d7f87ec65d7e74262513a92
MatthiasDeWinter <dewinter.matthias@gmail.com>
2015-02-18 13:38:40 +0000

    Update README and tests

--- tests/command/BackupCommandTest.php
+++ tests/command/BackupCommandTest.php
@@ -7,29 +7,9 @@ use Mockery as m;
 
 class BackupCommandTest extends TestCase {
 
-    private $databaseMock;
-    private $databaseBuilderMock;
-    private $tester;
-
     public function setUp()
     {
         parent::setUp();
-
-        $this->databaseMock = m::mock('Spatie\DatabaseBackup\DatabaseInterface');
-        $this->databaseBuilderMock = m::mock('Spatie\DatabaseBackup\DatabaseBuilder');
-
-        /*$this->databaseBuilderMock->shouldReceive('getDatabase')
-             ->once()
-             ->andReturn($this->databaseMock);*/
-
-        $command = new BackupCommand($this->databaseBuilderMock);
-
-        $this->tester = new CommandTester($command);
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-
     }
 
     public function tearDown()
