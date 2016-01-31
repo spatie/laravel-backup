976d323a7f6fd985479d7f4b5d2814425492e36f
freek <freek@spatie.be>
2016-01-31 12:28:28 +0100

    wip

--- src/FileFinder.php
+++ src/FileFinder.php
@@ -2,24 +2,47 @@
 
 namespace Spatie\Backup;
 
-use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Collection;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class FileFinder
 {
     /**
      * @var array
      */
-    protected $includeFilesAndDirectories;
+    protected $includeFilesAndDirectories = [];
     /**
      * @var array
      */
-    protected $excludeFilesAndDirectories;
+    protected $excludeFilesAndDirectories = [];
 
-    public function __construct(array $includeFilesAndDirectories, array $excludeFilesAndDirectories)
+    /**
+     * @param array|string $includeFilesAndDirectories
+     */
+    public function __construct($includeFilesAndDirectories)
     {
+        if (!is_array($includeFilesAndDirectories)) {
+            $includeFilesAndDirectories = [$includeFilesAndDirectories];
+        }
+
         $this->includeFilesAndDirectories = $includeFilesAndDirectories;
+    }
+
+    /**
+     * Do not included the given files and directories.
+     *
+     * @param array|string $excludeFilesAndDirectories
+     */
+    public function excludeFilesFrom($excludeFilesAndDirectories) : FileFinder
+    {
+        if (!is_array($excludeFilesAndDirectories)) {
+            $excludeFilesAndDirectories = [$excludeFilesAndDirectories];
+        }
+
         $this->excludeFilesAndDirectories = $excludeFilesAndDirectories;
+
+        return $this;
     }
 
     public function getSelectedFiles() : array
@@ -36,36 +59,42 @@ class FileFinder
 
         $filesToBeExcluded = $this->getAllFiles($this->excludeFilesAndDirectories);
 
-        return collect($filesToBeIncluded)->filter(function ($file) use ($filesToBeExcluded) {
+        $selectedFiles = collect($filesToBeIncluded)->filter(function (string $file) use ($filesToBeExcluded) {
             return !in_array($file, $filesToBeExcluded);
         })
         ->toArray();
+
+        $selectedFiles = array_values($selectedFiles);
+
+        return $selectedFiles;
     }
 
     /*
      * Make a unique array of all files from a given array of files and directories.
      */
-    protected function getAllFiles(array $files) : array
+    protected function getAllFiles(array $paths) : array
     {
-        collect($files)
-            ->filter(function (string $file) {
-                return Filesystem::isFile($file) || Filesystem::isDirectory($file);
+        $allFiles = collect($paths)
+            ->filter(function (string $path) {
+                return file_exists($path);
             })
-
-            ->reduce(function (Collection $allFiles, string $file) {
-                if (Filesystem::isDirectory($file)) {
-                    return $allFiles->merge($this->getAllFilesFromDirectory($file));
-                }
-
-                return $allFiles->push($file);
-            }, collect())
 
             ->map(function (string $file) {
-                return pathinfo($file, PATHINFO_FILENAME);
+                return realpath($file);
             })
+
+            ->reduce(function (Collection $filePaths, string $path) {
+                if (is_dir($path)) {
+                    return $filePaths->merge($this->getAllFilesFromDirectory($path));
+                }
+
+                return $filePaths->push($path);
+            }, collect())
 
             ->unique()
             ->toArray();
+
+        return $allFiles;
     }
 
     /*
@@ -79,6 +108,12 @@ class FileFinder
             ->files()
             ->in($directory);
 
-        return iterator_to_array($finder);
+        $filePaths = array_map(function (SplFileInfo $fileInfo) {
+            return $fileInfo->getPathname();
+        }, iterator_to_array($finder));
+
+        $filePaths = array_values($filePaths);
+
+        return $filePaths;
     }
 }
---
+++ tests/Unit/FileFinderTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Spatie\Skeleton\Test\Unit;
+
+use Spatie\Backup\FileFinder;
+
+class FileFinderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @string
+     */
+    protected $sourceDirectory;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->sourceDirectory = realpath(__DIR__.'/../testfiles/source');
+    }
+
+    /** @test */
+    public function it_can_get_all_the_files_in_a_directory_and_subdirectories()
+    {
+        $fileFinder = new FileFinder($this->sourceDirectory);
+
+        $this->assertSame(
+            $this->getTestFiles([
+                'directory1/directory1/file1.txt',
+                'directory1/directory1/file2.txt',
+                'directory1/file1.txt',
+                'directory1/file2.txt',
+                'directory2/directory1/file1.txt',
+                'file1.txt',
+                'file2.txt',
+                'file3.txt',
+            ]), $fileFinder->getSelectedFiles());
+    }
+
+    /** @test */
+    public function it_can_exclude_files_from_a_given_subdirectory()
+    {
+        $fileFinder = (new FileFinder($this->sourceDirectory))
+                        ->excludeFilesFrom("{$this->sourceDirectory}/directory1");
+
+        $this->getTestFiles([
+            'directory2/directory1/file1.txt',
+            'file1.txt',
+            'file2.txt',
+            'file3.txt',
+        ], $fileFinder->getSelectedFiles());
+    }
+
+    /** @test */
+    public function it_can_select_files_from_multiple_directories()
+    {
+        $fileFinder = (new FileFinder([
+            $this->sourceDirectory.'/directory1/directory1',
+            $this->sourceDirectory.'/directory2/directory1',
+        ]));
+
+        $this->assertSame(
+        $this->getTestFiles([
+            'directory1/directory1/file1.txt',
+            'directory1/directory1/file2.txt',
+            'directory2/directory1/file1.txt',
+        ]), $fileFinder->getSelectedFiles());
+    }
+
+    public function getTestFiles(array $relativePaths) : array
+    {
+        $absolutePaths = array_map(function (string $path) {
+             return "{$this->sourceDirectory}/{$path}";
+         }, $relativePaths);
+
+        return $absolutePaths;
+    }
+}
