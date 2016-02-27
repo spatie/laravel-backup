7e83115273ed0a8c2aa7a15d033971bdd7474260
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-02-27 18:04:56 +0100

    BaseCommand fix

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -9,10 +9,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class BaseCommand extends Command
 {
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int
+     */
     public function run(InputInterface $input, OutputInterface $output)
     {
         app(ConsoleOutput::class)->setOutput($this);
 
-        parent::run($input, $output);
+        return parent::run($input, $output);
     }
 }
