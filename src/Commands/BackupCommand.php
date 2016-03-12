0c0e4a6a58d6954fc1945a8f04cff85ae5680eae
freek <freek@spatie.be>
2016-03-12 18:14:24 +0100

    more tests

--- src/Commands/BackupCommand.php
+++ src/Commands/BackupCommand.php
@@ -12,7 +12,7 @@ class BackupCommand extends BaseCommand
     /**
      * @var string
      */
-    protected $signature = 'backup:run {--only-db} {--only-files} {--backup-only-to}';
+    protected $signature = 'backup:run {--only-db} {--only-files} {--backup-only-to-disk}';
 
     /**
      * @var string
@@ -36,8 +36,8 @@ class BackupCommand extends BaseCommand
                 $backupJob->doNotBackupDatabases();
             }
 
-            if ($this->option('backup-only-to')) {
-                $backupJob->backupOnlyTo($this->option('backup-only-to'));
+            if ($this->option('backup-only-to-disk')) {
+                $backupJob->backupOnlyTo($this->option('backup-only-to-disk'));
             }
 
             $backupJob->run();
