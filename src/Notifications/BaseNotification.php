50588f2f5b067bca2b8f9484ba9298d07f05dd45
freek <freek@spatie.be>
2016-09-05 15:57:00 +0200

    wip

--- src/Notifications/BaseNotification.php
+++ src/Notifications/BaseNotification.php
@@ -2,24 +2,10 @@
 
 namespace Spatie\Backup\Notifications;
 
-use Illuminate\Config\Repository;
 use Illuminate\Notifications\Notification;
 
 abstract class BaseNotification extends Notification
 {
-    /** @var \Spatie\Backup\Notifications\PackageUser */
-    protected $packageUser;
-
-    /** @var \Illuminate\Config\Repository */
-    protected $config;
-
-    public function __construct(PackageUser $packageUser, Repository $config)
-    {
-        $this->packageUser = $packageUser;
-
-        $this->config = $config;
-    }
-
     /**
      * Get the notification's delivery channels.
      *
@@ -28,6 +14,6 @@ abstract class BaseNotification extends Notification
      */
     public function via($notifiable)
     {
-        return $this->config->get('laravel-backup.notifications.events.' . static::class);
+        return config('laravel-backup.notifications.events.' . static::class);
     }
 }
--- src/Notifications/Notifications/BackupHasFailed.php
+++ src/Notifications/Notifications/BackupHasFailed.php
@@ -1,7 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: freek
- * Date: 24/08/16
- * Time: 16:10
- */
+
+namespace Spatie\Backup\Notifications\Notifications;
+
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Spatie\Backup\Notifications\BaseNotification;
+
+class BackupHasFailed extends BaseNotification
+{
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->success()
+            ->line('A backup was made! Hurray!');
+    }
+
+    public function toSlack($notifiable)
+    {
+        return (new SlackMessage)
+            ->success()
+            ->content('A backup was made! Hurray!');
+    }
+}
