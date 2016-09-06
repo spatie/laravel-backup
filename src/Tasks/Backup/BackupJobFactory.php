e63181933baf8eaddd3d15a1e7d752e9fea17ae5
freek <freek@spatie.be>
2016-09-06 22:47:40 +0200

    refactor db dumpers

--- src/Tasks/Backup/BackupJobFactory.php
+++ src/Tasks/Backup/BackupJobFactory.php
@@ -3,9 +3,6 @@
 namespace Spatie\Backup\Tasks\Backup;
 
 use Spatie\Backup\BackupDestination\BackupDestinationFactory;
-use Spatie\Backup\Exceptions\InvalidConfiguration;
-use Spatie\DbDumper\Databases\MySql;
-use Spatie\DbDumper\Databases\PostgreSql;
 
 class BackupJobFactory
 {
@@ -28,56 +25,8 @@ class BackupJobFactory
 
     protected static function getDbDumpers(array $dbConnectionNames): array
     {
-        $dbDumpers = array_map(function ($dbConnectionName) {
-            $dbConfig = config("database.connections.{$dbConnectionName}");
-
-            switch ($dbConfig['driver']) {
-                case 'mysql':
-                    $dbDumper = MySql::create()
-                        ->setHost($dbConfig['host'])
-                        ->setDbName($dbConfig['database'])
-                        ->setUserName($dbConfig['username'])
-                        ->setPassword($dbConfig['password'])
-                        ->setDumpBinaryPath(isset($dbConfig['dump_command_path']) ? $dbConfig['dump_command_path'] : '')
-                        ->setTimeout(isset($dbConfig['dump_command_timeout']) ? $dbConfig['dump_command_timeout'] : null);
-
-                    if (isset($dbConfig['port'])) {
-                        $dbDumper->setPort($dbConfig['port']);
-                    }
-
-                    if (isset($dbConfig['dump_using_single_transaction']) && $dbConfig['dump_using_single_transaction'] == true) {
-                        $dbDumper->useSingleTransaction();
-                    }
-
-                    return $dbDumper;
-                    break;
-
-                case 'pgsql':
-                    $dbDumper = PostgreSql::create()
-                        ->setHost($dbConfig['host'])
-                        ->setDbName($dbConfig['database'])
-                        ->setUserName($dbConfig['username'])
-                        ->setPassword($dbConfig['password'])
-                        ->setDumpBinaryPath(isset($dbConfig['dump_command_path']) ? $dbConfig['dump_command_path'] : '')
-                        ->setTimeout(isset($dbConfig['dump_command_timeout']) ? $dbConfig['dump_command_timeout'] : null);
-
-                    if (isset($dbConfig['dump_use_inserts']) && $dbConfig['dump_use_inserts'] == true) {
-                        $dbDumper->useInserts();
-                    }
-
-                    if (isset($dbConfig['port'])) {
-                        $dbDumper->setPort($dbConfig['port']);
-                    }
-
-                    return $dbDumper;
-                    break;
-
-                default:
-                    throw InvalidConfiguration::cannotUseUnsupportedDriver($dbConnectionName, $dbConfig['driver']);
-                    break;
-            }
-        }, $dbConnectionNames);
-
-        return $dbDumpers;
+        return collect($dbConnectionNames)->map(function(string $dbConnectionName) {
+           return DbDumperFactory::create($dbConnectionName);
+        });
     }
 }
---
+++ src/Tasks/Backup/DbDumperFactory.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Spatie\Backup\Tasks\Backup;
+
+class DbDumperFactory
+{
+    /**
+     * @param string $dbConnectionName ;
+     *
+     * @return mixed
+     */
+    public static function create(string $dbConnectionName)
+    {
+        $dbConfig = config("database.connections.{$dbConnectionName}");
+
+        $dbDumper = DbDumperFactory::create($dbConfig['driver'])
+            ->setHost($dbConfig['host'])
+            ->setDbName($dbConfig['database'])
+            ->setUserName($dbConfig['username'])
+            ->setPassword($dbConfig['password'])
+            ->setDumpBinaryPath($dbConfig['dump_command_path'] ?? '')
+            ->setTimeout(isset($dbConfig['dump_command_timeout']) ?? 0);
+
+        if (isset($dbConfig['dump'])) {
+            $dbDumper = static::processExtraDumpParameters($dbConfig['dump'], $dbDumper);
+        };
+
+        return $dbDumper;
+    }
+
+    /**
+     * @param array $dumpConfiguration
+     *
+     * @param $dbDumper
+     *
+     * @return mixed
+     */
+    protected static function processExtraDumpParameters(array $dumpConfiguration, $dbDumper)
+    {
+        collect($dumpConfiguration)->filter(function (string $configValue, string $configName) use ($dbDumper) {
+            return method_exists($dbDumper, self::getDumperMethodName($configName));
+        })->each(function (string $configValue, string $configName) use ($dbDumper)  {
+            $methodName = self::getDumperMethodName($configName);
+
+            $dbDumper->$methodName($configValue);
+        });
+
+        return $dbDumper;
+    }
+
+    protected function getDumperMethodName(string $configName): string
+    {
+        return 'set' . studly_case($configName);
+    }
+
+}
+
