ecb7d395a802fce142c97013a53f56ac92d04da8
Vladimir Yuldashev <misterio92@gmail.com>
2016-09-21 16:27:57 +0300

    Use application name instead of url

--- config/laravel-backup.php
+++ config/laravel-backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_URL'),
+        'name' => config('app.name'),
 
         'source' => [
 
