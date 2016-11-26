bfccfb0beb6a809e1cba2a5ce756dfe34646e51e
Marcel Pociot <m.pociot@gmail.com>
2016-11-26 16:09:54 +0100

    Make backup filename prefix configurable (#295)

--- config/laravel-backup.php
+++ config/laravel-backup.php
@@ -49,6 +49,11 @@ return [
         'destination' => [
 
             /*
+             * The filename prefix used for the backup zip file.
+             */
+            'filename_prefix' => '',
+
+            /*
              * The disk names on which the backups will be stored.
              */
             'disks' => [
