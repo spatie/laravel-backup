bb5a18e2d7ed72854a93789932807415989d3eb0
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-02-02 17:24:44 +0100

    StyleCI

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -188,6 +188,7 @@ abstract class TestCase extends Orchestra
         if ($zip->open($zipPath) === true) {
             return $zip->locateName($filename, \ZipArchive::FL_NODIR) !== false;
         }
+
         return false;
     }
 }
