ffe024ac57f63b60a8e1db4b63bad0ac32bcce26
freek <freek@spatie.be>
2017-09-29 09:18:29 +0200

    use APP_NAME instead of APP_URL by default

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_URL'),
+        'name' => env('APP_NAME'),
 
         'source' => [
 
