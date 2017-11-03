f094fd8ef4b276fc68d6043ff296ee0af2395ea7
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-11-03 14:11:18 +0100

    Add failing test for deleting backups when reaching max storage

--- tests/Integration/CleanupCommandTest.php
+++ tests/Integration/CleanupCommandTest.php
@@ -28,15 +28,23 @@ class CleanupCommandTest extends TestCase
     public function it_can_remove_old_backups_until_using_less_than_maximum_storage()
     {
         // 1 megabyte storage size
-        $this->app['config']->set('backup.cleanup.defaultStrategy.deleteOldestBackupsWhenUsingMoreMegabytesThan', 1);
+        $this->app['config']->set('backup.cleanup.defaultStrategy.deleteOldestBackupsWhenUsingMoreMegabytesThan', 2);
 
         $this->testHelper->createTempFile1Mb('mysite.com/test1.zip', Carbon::now()->subDays(1));
         $this->testHelper->createTempFile1Mb('mysite.com/test2.zip', Carbon::now()->subDays(2));
+        $this->testHelper->createTempFile1Mb('mysite.com/test3.zip', Carbon::now()->subDays(3));
+        $this->testHelper->createTempFile1Mb('mysite.com/test4.zip', Carbon::now()->subDays(4));
 
         Artisan::call('backup:clean');
 
         $this->assertTempFilesExist([
             'mysite.com/test1.zip',
+            'mysite.com/test2.zip',
+        ]);
+
+        $this->assertTempFilesNotExist([
+            'mysite.com/test3.zip',
+            'mysite.com/test4.zip',
         ]);
     }
 
