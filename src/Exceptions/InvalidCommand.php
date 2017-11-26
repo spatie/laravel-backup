2d9899d2a9780e82a426623ff6f3211b9c0ee1c5
Freek Van der Herten <freek@spatie.be>
2017-11-26 22:26:09 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidCommand.php
+++ src/Exceptions/InvalidCommand.php
@@ -6,7 +6,7 @@ use Exception;
 
 class InvalidCommand extends Exception
 {
-    public static function create(string $reason): InvalidCommand
+    public static function create(string $reason): self
     {
         return new static($reason);
     }
