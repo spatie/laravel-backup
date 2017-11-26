a757ad02eeea14f6d19473b19240b65cfb4cd2e7
Freek Van der Herten <freek@spatie.be>
2017-11-26 22:29:35 +0000

    Apply fixes from StyleCI

--- src/Exceptions/CannotCreateDbDumper.php
+++ src/Exceptions/CannotCreateDbDumper.php
@@ -6,7 +6,7 @@ use Exception;
 
 class CannotCreateDbDumper extends Exception
 {
-    public static function unsupportedDriver(string $driver): CannotCreateDbDumper
+    public static function unsupportedDriver(string $driver): self
     {
         return new static("Cannot create a dumper for db driver `{$driver}`. Use `mysql`, `pgsql`, `mongodb` or `sqlite`.");
     }
