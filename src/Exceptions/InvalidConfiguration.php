a67a3557bc8011fd45134888687853ff572afb3e
Freek Van der Herten <freek@spatie.be>
2017-11-27 08:25:40 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidConfiguration.php
+++ src/Exceptions/InvalidConfiguration.php
@@ -6,7 +6,7 @@ use Exception;
 
 class InvalidConfiguration extends Exception
 {
-    public static function cannotUseUnsupportedDriver(string $connectionName, string $driverName): InvalidConfiguration
+    public static function cannotUseUnsupportedDriver(string $connectionName, string $driverName): self
     {
         return new static("Db connection `{$connectionName}` uses an unsupported driver `{$driverName}`. Only `mysql` and `pgsql` are supported.");
     }
