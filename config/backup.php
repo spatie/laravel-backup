5ff1db7a882929dcad3eeb2cc9a4282275339cbd
Krzysztof Gwiazda <krzysztof.gwiazda@agsystems.pl>
2018-06-18 10:16:02 +0200

    Change exclude sources

--- config/backup.php
+++ config/backup.php
@@ -29,6 +29,8 @@ return [
                 'exclude' => [
                     base_path('vendor'),
                     base_path('node_modules'),
+                    base_path('.git'),
+                    storage_path(),
                 ],
 
                 /*
