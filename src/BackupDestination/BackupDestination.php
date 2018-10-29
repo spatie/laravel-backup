0674b25e2d9dc711e36be677898f74447173fc16
Paco Beumer <p.beumer@gmx.net>
2018-10-29 21:57:37 +0100

    clear the backup-size cache
    when running the `artisan backup:clean` command a wrong backup size is
    shown due to caching. Fixes #776

--- src/BackupDestination/BackupDestination.php
+++ src/BackupDestination/BackupDestination.php
@@ -155,4 +155,11 @@ class BackupDestination
 
         return $newestBackup->date()->gt($date);
     }
+
+    public function fresh(): BackupDestination
+    {
+        $this->backupCollectionCache = null;
+
+        return $this;
+    }
 }
