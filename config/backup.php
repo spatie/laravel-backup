77cd962a71be0ed6d5349de15c621830dc32909e
freek <freek@spatie.be>
2018-11-12 13:19:15 +0100

    use env var

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => config('app.name'),
+        'name' => env('APP_NAME'),
 
         'source' => [
 
