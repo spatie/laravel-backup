061cb781132309bc7a1d0416400d3c4d40cc8bbc
Vitaly Yushkevich <yushkevichv@gmail.com>
2018-12-24 01:17:53 +0300

    fix rename option param name

--- tests/Integration/BackupDestination/BackupTest.php
+++ tests/Integration/BackupDestination/BackupTest.php
@@ -68,7 +68,7 @@ class BackupTest extends TestCase
         $this->app['config']->set('filesystems.disks.s3-test-backup', [
             'driver' => 's3',
 
-            'dump_extra_options' => [
+            'backup_extra_options' => [
                 'StorageClass' => 'COLD',
             ],
         ]);
