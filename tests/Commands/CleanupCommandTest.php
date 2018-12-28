07d90d39306b86844232af6f56b2b1ef0cdd58c3
Freek Van der Herten <freek@spatie.be>
2018-12-28 16:26:37 +0000

    Apply fixes from StyleCI

--- tests/Commands/CleanupCommandTest.php
+++ tests/Commands/CleanupCommandTest.php
@@ -2,12 +2,12 @@
 
 namespace Spatie\Backup\Tests\Commands;
 
+use Spatie\Backup\Tests\TestCase;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Backup\Events\CleanupWasSuccessful;
-use Spatie\Backup\Tests\TestCase;
 
 class CleanupCommandTest extends TestCase
 {
@@ -131,8 +131,9 @@ class CleanupCommandTest extends TestCase
     /** @test */
     public function it_will_never_delete_the_newest_backup()
     {
-        $backupPaths = collect(range(5,10))->map(function(int $numberOfYears) {
+        $backupPaths = collect(range(5, 10))->map(function (int $numberOfYears) {
             $date = now()->subYears($numberOfYears);
+
             return $this->createFileOnDisk('local', "mysite/test_{$date->format('Ymd')}.zip", $date);
         });
 
@@ -142,7 +143,7 @@ class CleanupCommandTest extends TestCase
 
         $backupPaths->shift();
 
-        $backupPaths->each(function(string $path) {
+        $backupPaths->each(function (string $path) {
             Storage::disk('local')->assertMissing($path);
         });
     }
@@ -150,7 +151,6 @@ class CleanupCommandTest extends TestCase
     /** @test */
     public function it_should_trigger_the_cleanup_successful_event()
     {
-
         $this->artisan('backup:clean')->assertExitCode(0);
 
         Event::assertDispatched(CleanupWasSuccessful::class);
