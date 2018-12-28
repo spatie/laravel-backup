a54e029a05893cda8d9116da77ae51603a03a36a
freek <freek@spatie.be>
2018-12-28 09:51:03 +0100

    nitpick

--- src/Exceptions/InvalidBackupDestination.php
+++ src/Exceptions/InvalidBackupDestination.php
@@ -8,6 +8,6 @@ class InvalidBackupDestination extends Exception
 {
     public static function diskNotSet(string $backupName): self
     {
-        return new static("There is no disk set for the backup named {$backupName}");
+        return new static("There is no disk set for the backup named `{$backupName}`.");
     }
 }
