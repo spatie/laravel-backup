76c15ffcde14b9404184effb8b7f946cfc89b4b5
freek <freek@spatie.be>
2018-12-28 16:17:11 +0100

    simplify test

--- tests/HealthChecks/MaximumStorageInMegabytesTest.php
+++ tests/HealthChecks/MaximumStorageInMegabytesTest.php
@@ -3,6 +3,7 @@
 namespace Spatie\Backup\Tests\HealthChecks;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Event;
 use Spatie\Backup\Tests\TestCase;
 use Spatie\Backup\Events\HealthyBackupWasFound;
 use Spatie\Backup\Events\UnhealthyBackupWasFound;
@@ -14,8 +15,8 @@ class MaximumStorageInMegabytesTest extends TestCase
     {
         parent::setUp();
 
-        $this->testHelper->initializeTempDirectory();
-
+        Event::fake();
+        
         config()->set('backup.monitor_backups.0.health_checks', [
             MaximumStorageInMegabytes::class => 1,
         ]);
@@ -24,21 +25,22 @@ class MaximumStorageInMegabytesTest extends TestCase
     /** @test */
     public function it_succeeds_when_a_fresh_backup_is_present()
     {
-        $this->expectsEvents(HealthyBackupWasFound::class);
+        $this->createFile1MbOnDisk('local','mysite/test.zip', now());
 
-        $this->testHelper->createTempFile1Mb('mysite/test.zip', now());
+        $this->artisan('backup:monitor')->assertExitCode(0);
 
-        Artisan::call('backup:monitor');
+        Event::assertDispatched(HealthyBackupWasFound::class);
     }
 
     /** @test */
     public function it_fails_when_max_mb_has_been_exceeded()
     {
-        $this->testHelper->createTempFile1Mb('mysite/test_1.zip', now()->subSeconds(2));
-        $this->testHelper->createTempFile1Mb('mysite/test_2.zip', now()->subSeconds(1));
+        $this->createFile1MbOnDisk('local','mysite/test_1.zip', now()->subSeconds(2));
+        $this->createFile1MbOnDisk('local','mysite/test_2.zip', now()->subSeconds(1));
 
-        $this->expectsEvents(UnhealthyBackupWasFound::class);
+        $this->artisan('backup:monitor')->assertExitCode(0);
 
-        Artisan::call('backup:monitor');
+        Event::assertDispatched(UnhealthyBackupWasFound::class);
+
     }
 }
