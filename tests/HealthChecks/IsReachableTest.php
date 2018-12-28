c575219d6fec4f81951be62e67b97b1f264416f0
freek <freek@spatie.be>
2018-12-28 16:12:00 +0100

    simplify test

--- tests/HealthChecks/IsReachableTest.php
+++ tests/HealthChecks/IsReachableTest.php
@@ -2,30 +2,35 @@
 
 namespace Spatie\Backup\Tests\HealthChecks;
 
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Event;
+use Spatie\Backup\Events\UnhealthyBackupWasFound;
 use Spatie\Backup\Tests\TestCase;
 use Spatie\Backup\Events\HealthyBackupWasFound;
-use Spatie\Backup\Events\UnhealthyBackupWasFound;
 
 class IsReachableTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        Event::fake();
+    }
+
     /** @test */
     public function it_succeeds_when_destination_is_reachable()
     {
-        $this->testHelper->initializeTempDirectory();
+        $this->artisan('backup:monitor')->assertExitCode(0);
 
-        $this->expectsEvents(HealthyBackupWasFound::class);
-
-        Artisan::call('backup:monitor');
+        Event::assertDispatched(HealthyBackupWasFound::class);
     }
 
     /** @test */
     public function it_fails_when_backup_destination_is_not_reachable()
     {
-        config()->set('filesystems.disks.local.root', '/foo/bar');
+        config()->set('backup.monitor_backups.0.disks', ['nonExistingDisk']);
 
-        $this->expectsEvents(UnhealthyBackupWasFound::class);
+        $this->artisan('backup:monitor')->assertExitCode(0);
 
-        Artisan::call('backup:monitor');
+        Event::assertDispatched(UnhealthyBackupWasFound::class);
     }
 }
