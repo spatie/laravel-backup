700d2361c9ec6786431c62cd94ad99e03238ccdf
Freek Van der Herten <freek@spatie.be>
2019-01-03 16:44:49 +0100

    Update backup.php

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME'),
+        'name' => env('APP_NAME', 'laravel-backup'),
 
         'source' => [
 
