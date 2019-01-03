0fa5e6a155e98cbe4b9de87399fedfdadcd90f8f
Mark van den Broek <mvdnbrk@gmail.com>
2019-01-03 16:47:08 +0100

    Adds a default value for APP_NAME.

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME'),
+        'name' => env('APP_NAME', 'Backup'),
 
         'source' => [
 
@@ -145,7 +145,7 @@ return [
      */
     'monitor_backups' => [
         [
-            'name' => env('APP_NAME'),
+            'name' => env('APP_NAME', 'Backup'),
             'disks' => ['local'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
