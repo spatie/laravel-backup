b50d0ac32b03ce109174f8752d7a4134c2d7a798
Otto Rottier <ottorottier@gmail.com>
2019-06-25 20:40:52 +0200

    Prevent excessive IO calls by caching Backup properties

--- src/BackupDestination/Backup.php
+++ src/BackupDestination/Backup.php
@@ -13,6 +13,15 @@ class Backup
     /** @var string */
     protected $path;
 
+    /** @var bool */
+    protected $exists;
+
+    /** @var Carbon */
+    protected $date;
+
+    /** @var int */
+    protected $size;
+
     public function __construct(Filesystem $disk, string $path)
     {
         $this->disk = $disk;
@@ -27,12 +36,21 @@ class Backup
 
     public function exists(): bool
     {
-        return $this->disk->exists($this->path);
+        if ($this->exists === null) {
+            $this->exists = $this->disk->exists($this->path);
+        }
+
+        return $this->exists;
     }
 
     public function date(): Carbon
     {
-        return Carbon::createFromTimestamp($this->disk->lastModified($this->path));
+        if ($this->date === null) {
+            $this->date = Carbon::createFromTimestamp($this->disk->lastModified($this->path));
+            $this->exists = true;
+        }
+
+        return $this->date;
     }
 
     /**
@@ -40,11 +58,16 @@ class Backup
      */
     public function size(): int
     {
-        if (! $this->exists()) {
-            return 0;
+        if ($this->size === null) {
+            if (! $this->exists()) {
+                return 0;
+            }
+
+            $this->size = $this->disk->size($this->path);
+            $this->exists = true;
         }
 
-        return $this->disk->size($this->path);
+        return $this->size;
     }
 
     public function stream()
@@ -54,6 +77,10 @@ class Backup
 
     public function delete()
     {
+        $this->exists = null;
+        $this->date = null;
+        $this->size = null;
+
         $this->disk->delete($this->path);
 
         consoleOutput()->info("Deleted backup `{$this->path}`.");
