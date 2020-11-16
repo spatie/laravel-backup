2dce830fc77cd68916287dd4a4a72257f5269b9f
Niels <niels@spatie.be>
2020-11-16 16:11:40 +0100

    require higher version of mockery

--- tests/DbDumperFactoryTest.php
+++ tests/DbDumperFactoryTest.php
@@ -155,7 +155,6 @@ class DbDumperFactoryTest extends TestCase
         $this->assertEquals('myDb-read', $dumper->getDbName());
     }
 
-    /** @test */
     public function it_will_throw_an_exception_when_creating_an_unknown_type_of_dumper()
     {
         $this->expectException(CannotCreateDbDumper::class);
