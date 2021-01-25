09fdf0da8830c7bcd1022918642efb1dcd527dd4
freek <freek@spatie.be>
2021-01-25 15:47:17 +0100

    wip

--- src/BackupServiceProvider.php
+++ src/BackupServiceProvider.php
@@ -10,42 +10,31 @@ use Spatie\Backup\Commands\MonitorCommand;
 use Spatie\Backup\Helpers\ConsoleOutput;
 use Spatie\Backup\Notifications\EventHandler;
 use Spatie\Backup\Tasks\Cleanup\CleanupStrategy;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class BackupServiceProvider extends ServiceProvider
+class BackupServiceProvider extends PackageServiceProvider
 {
-    public function boot()
+    public function configurePackage(Package $package): void
     {
-        $this->publishes([
-            __DIR__.'/../config/backup.php' => config_path('backup.php'),
-        ], 'config');
-
-        $this->publishes([
-            __DIR__.'/../resources/lang' => "{$this->app['path.lang']}/vendor/backup",
-        ]);
-
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang/', 'backup');
+        $package
+            ->name('laravel-backup')
+            ->hasConfigFile()
+            ->hasTranslations()
+            ->hasCommands([
+                BackupCommand::class,
+                CleanupCommand::class,
+                ListCommand::class,
+                MonitorCommand::class,
+            ]);
     }
 
-    public function register()
+    public function packageRegistered()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/backup.php', 'backup');
-
         $this->app['events']->subscribe(EventHandler::class);
 
-        $this->app->bind('command.backup:run', BackupCommand::class);
-        $this->app->bind('command.backup:clean', CleanupCommand::class);
-        $this->app->bind('command.backup:list', ListCommand::class);
-        $this->app->bind('command.backup:monitor', MonitorCommand::class);
+        $this->app->singleton(ConsoleOutput::class);
 
         $this->app->bind(CleanupStrategy::class, config('backup.cleanup.strategy'));
-
-        $this->commands([
-            'command.backup:run',
-            'command.backup:clean',
-            'command.backup:list',
-            'command.backup:monitor',
-        ]);
-
-        $this->app->singleton(ConsoleOutput::class);
     }
 }
