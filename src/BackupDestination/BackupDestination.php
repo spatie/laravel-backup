191f66fc0c69cd3a3d077fe13dac95efb1bb8a6e
freekmurze <freekmurze@users.noreply.github.com>
2021-01-29 07:29:53 +0000

    Fix styling

--- src/BackupDestination/BackupDestination.php
+++ src/BackupDestination/BackupDestination.php
@@ -110,12 +110,13 @@ class BackupDestination
 
         $files = [];
 
-        if (!is_null($this->disk)) {
+        if (! is_null($this->disk)) {
             // $this->disk->allFiles() may fail when $this->disk is not reachable
             // in that case we still want to send the notification
             try {
                 $files = $this->disk->allFiles($this->backupName);
-            } catch (Exception $ex) {}
+            } catch (Exception $ex) {
+            }
         }
 
         return $this->backupCollectionCache = BackupCollection::createFromFiles(
