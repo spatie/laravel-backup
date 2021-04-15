15cdcb3752991e401abb2a7a5c0b7da42f3dc025
Freek Van der Herten <freek@spatie.be>
2021-04-15 11:36:50 +0200

    Update Notifiable.php

--- src/Notifications/Notifiable.php
+++ src/Notifications/Notifiable.php
@@ -8,7 +8,7 @@ class Notifiable
 {
     use NotifiableTrait;
 
-    public function routeNotificationForMail(): string
+    public function routeNotificationForMail(): string|array
     {
         return config('backup.notifications.mail.to');
     }
