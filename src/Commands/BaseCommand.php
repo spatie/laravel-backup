05f60e53f651cdf8104c6e4f991ea35f07170cf1
Tyler Vigario <TylerVigario90@gmail.com>
2021-04-21 10:13:14 -0700

    Avoid SIGINT on Windows

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -9,7 +9,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class BaseCommand extends SignalAwareCommand
 {
-    protected array $handlesSignals = [SIGINT];
+    protected array $handlesSignals = [];
+
+    public function __construct()
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+            $this->handlesSignals[] = SIGINT;
+        }
+
+        parent::__construct();
+    }
 
     public function run(InputInterface $input, OutputInterface $output): int
     {
