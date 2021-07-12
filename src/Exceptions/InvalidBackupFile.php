837c663f1776a9bb6a4301e4c20726c40c9612a8
Aju Chacko <ajuchacko91@gmail.com>
2021-07-12 17:59:48 +0530

    fix: grammatical error on exception message

--- src/Exceptions/InvalidBackupFile.php
+++ src/Exceptions/InvalidBackupFile.php
@@ -9,13 +9,13 @@ class InvalidBackupFile extends Exception
 {
     public static function writeError(string $backupName): self
     {
-        return new static("There is have been error writing file for the backup named `{$backupName}`.");
+        return new static("There has been an error writing file for the backup named `{$backupName}`.");
     }
 
     public static function readError(Backup $backup): self
     {
         $backupName = basename($backup->path());
 
-        return new static("There is have been error reading file for the backup named `{$backupName}`");
+        return new static("There has been an error reading file for the backup named `{$backupName}`");
     }
 }
