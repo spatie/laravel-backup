aeb9d1e8fef8baa66e271b79bf0f4f0e1b995f6b
Bryan Zarzuela <bryan@teleserv.ph>
2022-03-01 06:54:23 +0800

    Adds testing for the fallback to the old config keys

--- tests/Commands/MonitorCommandTest.php
+++ tests/Commands/MonitorCommandTest.php
@@ -4,18 +4,8 @@ namespace Spatie\Backup\Tests\Commands;
 
 use Spatie\Backup\Tests\TestCase;
 
-class ListCommandTest extends TestCase
+class MonitorCommandTest extends TestCase
 {
-    /** @test */
-    public function it_can_run_the_list_command()
-    {
-        config()->set('backup.backup.destination.disks', [
-            'local',
-        ]);
-
-        $this->artisan('backup:list')->assertExitCode(0);
-    }
-
     /** @test */
     function it_falls_back_to_the_old_config_key_gracefully()
     {
@@ -27,6 +17,6 @@ class ListCommandTest extends TestCase
 
         config(['backup' => $config_values]);
 
-        $this->artisan('backup:list')->assertExitCode(0);
+        $this->artisan('backup:monitor')->assertExitCode(0);
     }
 }
