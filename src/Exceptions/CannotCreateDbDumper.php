c0f5f7be4cb4a46d75d3623492e34e3410bca1f8
Johan de Jager <johan@laptify.nl>
2023-04-25 02:22:47 +0200

    Show accurate list of supported drivers instead of the default Laravel driver names.

--- src/Exceptions/CannotCreateDbDumper.php
+++ src/Exceptions/CannotCreateDbDumper.php
@@ -8,6 +8,12 @@ class CannotCreateDbDumper extends Exception
 {
     public static function unsupportedDriver(string $driver): self
     {
-        return new static("Cannot create a dumper for db driver `{$driver}`. Use `mysql`, `pgsql`, `mongodb` or `sqlite`.");
+        $supportedDrivers = collect(config("database.connections"))->keys();
+
+        $formattedSupportedDrivers = $supportedDrivers
+            ->map(fn (string $supportedDriver) => "`$supportedDriver`")
+            ->join(glue: ', ', finalGlue: ' or ');
+
+        return new static("Cannot create a dumper for db driver `{$driver}`. Use {$formattedSupportedDrivers}.");
     }
 }
