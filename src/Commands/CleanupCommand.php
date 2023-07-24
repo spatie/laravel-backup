20f3236ec1f4889c1e1564c9c885db9f03ea7764
Yoan Ballesteros <y.ballesteros@presta-module.com>
2023-07-24 17:52:04 +0200

    - Introduce a retry mechanism for the cleanup command, in case an exception is encountered

--- src/Commands/CleanupCommand.php
+++ src/Commands/CleanupCommand.php
@@ -11,12 +11,16 @@ use Spatie\Backup\Tasks\Cleanup\CleanupStrategy;
 class CleanupCommand extends BaseCommand
 {
     /** @var string */
-    protected $signature = 'backup:clean {--disable-notifications}';
+    protected $signature = 'backup:clean {--disable-notifications} {--tries=}';
 
     /** @var string */
     protected $description = 'Remove all backups older than specified number of days in config.';
 
     protected CleanupStrategy $strategy;
+
+    protected int $tries = 1;
+
+    protected int $currentTry = 1;
 
     public function __construct(CleanupStrategy $strategy)
     {
@@ -27,13 +31,19 @@ class CleanupCommand extends BaseCommand
 
     public function handle()
     {
-        consoleOutput()->comment('Starting cleanup...');
+        consoleOutput()->comment($this->currentTry > 1 ? sprintf('Attempt n°%d...', $this->currentTry) : 'Starting cleanup...');
 
         $disableNotifications = $this->option('disable-notifications');
 
-        try {
-            $config = config('backup');
+        $config = config('backup');
 
+        if ($this->option('tries')) {
+            $this->tries = (int)$this->option('tries');
+        } elseif (!empty($config['cleanup']['tries'])) {
+            $this->tries = (int)$config['cleanup']['tries'];
+        }
+
+        try {
             $backupDestinations = BackupDestinationFactory::createFromArray($config['backup']);
 
             $cleanupJob = new CleanupJob($backupDestinations, $this->strategy, $disableNotifications);
@@ -42,6 +52,15 @@ class CleanupCommand extends BaseCommand
 
             consoleOutput()->comment('Cleanup completed!');
         } catch (Exception $exception) {
+            if ($this->tries > 1 && $this->currentTry < $this->tries) {
+                if (!empty($config['cleanup']['retry_delay'])) {
+                    sleep((int)$config['cleanup']['retry_delay']);
+                }
+
+                $this->currentTry += 1;
+                return $this->handle();
+            }
+
             if (! $disableNotifications) {
                 event(new CleanupHasFailed($exception));
             }
