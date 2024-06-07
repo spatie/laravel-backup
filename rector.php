0c66d929fc9cda8c5edbce9367a17fd33e92d863
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-06-07 10:13:18 +0200

    add code styling config

--- rector.php
+++ rector.php
@@ -3,24 +3,32 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\ClassMethod\OptionalParametersAfterRequiredRector;
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
+use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\If_\NullableCompareToNullRector;
+use Rector\CodingStyle\Rector\Plus\UseIncrementAssignRector;
+use Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
 return RectorConfig::configure()
-    ->withPaths([
-        __DIR__.'/config',
-        __DIR__.'/resources',
-        __DIR__.'/src',
-        __DIR__.'/tests',
-    ])
+    ->withPaths(['config', 'resources', 'src', 'tests'])
     ->withPhpSets(php81: true)
-    ->withRules([
-        AddVoidReturnTypeWhereNoReturnRector::class,
-    ])
+    ->withPreparedSets(deadCode: true,codingStyle: true)
     ->withSkip([
         ReturnNeverTypeRector::class,
         OptionalParametersAfterRequiredRector::class,
         ClosureToArrowFunctionRector::class,
+        FlipTypeControlToUseExclusiveTypeRector::class,
+        ExplicitBoolCompareRector::class,
+        EncapsedStringsToSprintfRector::class,
+        StaticClosureRector::class,
+        StaticArrowFunctionRector::class,
+        UseIncrementAssignRector::class,
+        PostIncDecToPreIncDecRector::class,
+        NullableCompareToNullRector::class,
     ]);
