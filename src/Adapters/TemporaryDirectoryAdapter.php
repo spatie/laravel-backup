16086981bfdd4cfc9cf3e772e4a13310c1a1a86c
Jon Erickson <jon@deschutesdesigngroup.com>
2024-09-30 20:11:26 -0700

    Revert an oopsie

--- src/Adapters/TemporaryDirectoryAdapter.php
+++ src/Adapters/TemporaryDirectoryAdapter.php
@@ -5,17 +5,7 @@ namespace Spatie\Backup\Adapters;
 use Spatie\Backup\Contracts\TemporaryDirectory;
 use Spatie\TemporaryDirectory\TemporaryDirectory as BaseTemporaryDirectory;
 
-class TemporaryDirectoryAdapter implements TemporaryDirectory
+class TemporaryDirectoryAdapter extends BaseTemporaryDirectory implements TemporaryDirectory
 {
-    protected BaseTemporaryDirectory $temporaryDirectory;
 
-    public function __construct(string $location = '')
-    {
-        $this->temporaryDirectory = new BaseTemporaryDirectory($location);
-    }
-
-    public function __call(string $name, array $arguments): mixed
-    {
-        return $this->temporaryDirectory->{$name}(...$arguments);
-    }
 }
