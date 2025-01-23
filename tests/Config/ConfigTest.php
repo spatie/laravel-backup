f5a238f54e53303d98871feda2f451b7cdbfea67
pxpm <pxpm88@gmail.com>
2025-01-23 12:22:07 +0000

    merge published config with package config

--- tests/Config/ConfigTest.php
+++ tests/Config/ConfigTest.php
@@ -3,6 +3,7 @@
 use Spatie\Backup\Config\BackupConfig;
 use Spatie\Backup\Config\CleanupConfig;
 use Spatie\Backup\Config\Config;
+use Spatie\Backup\Config\DestinationConfig;
 use Spatie\Backup\Config\MonitoredBackupsConfig;
 use Spatie\Backup\Config\NotificationsConfig;
 
@@ -34,4 +35,22 @@ it('receives temp directory as configured from service container', function () {
     $tempDirectory = app()->make('backup-temporary-project');
 
     expect($tempDirectory->path())->toBe('/foo');
+});
+
+it('merges the published config file with package config file', function () {
+    config()->set('backup.backup.destination', []);
+
+    $config = Config::fromArray(config('backup'));
+
+    expect($config->backup->destination)->toBeInstanceOf(DestinationConfig::class);
+    expect($config->backup->destination->compressionMethod)->toBe(-1);
+});
+
+it('merges the published config file with package config file and preserve published config values', function () {
+    config()->set('backup.backup.destination', ['compression_method' => 2]);
+
+    $config = Config::fromArray(config('backup'));
+
+    expect($config->backup->destination)->toBeInstanceOf(DestinationConfig::class);
+    expect($config->backup->destination->compressionMethod)->toBe(2);
 });
