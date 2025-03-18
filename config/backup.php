586ab4877abfe94a9973fd1433f10976977ff77b
Zacharias Creutznacher <zacharias.creutznacher@gmail.com>
2025-03-18 11:13:39 +0100

    Update backup.php
    use DB_CONNECTION env var as default database connection

--- config/backup.php
+++ config/backup.php
@@ -77,7 +77,7 @@ return [
              * For a complete list of available customization options, see https://github.com/spatie/db-dumper
              */
             'databases' => [
-                'mysql',
+                env('DB_CONNECTION', 'mysql'),
             ],
         ],
 
